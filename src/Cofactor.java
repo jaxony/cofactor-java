68dd16ea8df8be3927d9667ca05c74f488f7e6c3
Jackson Huang <jackson_huang@live.com>
2018-09-27 13:52:52 +0900

    Added constructor

--- src/Cofactor.java
+++ src/Cofactor.java
@@ -1,3 +1,27 @@
 public class Cofactor {
+    private int components;
+    private int maxIter;
+    private int batchSize;
+    private float initStd;
+    private float lamTheta;
+    private float lamBeta;
+    private float lamGamma;
+    private float c0;
+    private float c1;
+
+    public Cofactor(int components, int maxIter, int batchSize, float initStd,
+                    float lamTheta, float lamBeta, float lamGamma, float c0, float c1) {
+        this.components = components;
+        this.maxIter = maxIter;
+        this.batchSize = batchSize;
+        this.initStd = initStd;
+        this.lamTheta = lamTheta;
+        this.lamBeta = lamBeta;
+        this.lamGamma = lamGamma;
+        this.c0 = c0;
+        this.c1 = c1;
+        assert c0 < c1: "c0 must be smaller than c1";
+    }
+
 
 }
